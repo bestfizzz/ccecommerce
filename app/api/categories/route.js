21f125c13bab9b69908beb5ebda09b6d64cee255
bestfizzz <minhhoangnguyen19122004@gmail.com>
2024-05-26 00:09:26 +0700

    das

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -3,7 +3,6 @@ import { getCategories } from "@/libs/categories"
 export async function GET(req) {
     try {
         let res = await getCategories();
-        console.log(res);
         return Response.json(res);
     } catch (error) {
         console.error("Error fetching categories:", error);
