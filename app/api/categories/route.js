225d6ca3e636fa251fe432216c4a55245b3355d4
bestfizzz <minhhoangnguyen19122004@gmail.com>
2024-05-25 23:36:06 +0700

    Update route.js

--- app/api/categories/route.js
+++ app/api/categories/route.js
@@ -3,6 +3,7 @@ import { getCategories } from "@/libs/categories"
 export async function GET(req) {
     try {
         let res = await getCategories();
+        console.log(res);
         return Response.json(res);
     } catch (error) {
         console.error("Error fetching categories:", error);
