6d342280b6c732a51489bc873f49a35ff4fbf1bc
bestfizzz <minhhoangnguyen19122004@gmail.com>
2024-05-25 23:52:09 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 };
 
 export const getCategoriesData = async ()=>{
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/categories`, {method: 'GET'},{ next: { revalidate: 3600 } });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/products?get=all`, {method: 'GET'},{ next: { revalidate: 3600 } });
   return res.json()
 }
 
