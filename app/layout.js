9cddfd98696466f4fb28966d55ba0609048584d4
bestfizzz <minhhoangnguyen19122004@gmail.com>
2024-06-19 18:05:44 +0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ export default async function RootLayout({ children }) {
   const categoryList = await getCategories()///fucking nextjs npm run build on production doesnt serve api routes
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={inter.className + 'bg-white'}>
         <AuthProvider>
         <CartContextProvider>
